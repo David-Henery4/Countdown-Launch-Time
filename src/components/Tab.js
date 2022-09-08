c0e63ffe9053bf0e71280ea90fa4f9cd8283f50c
DizzlyD <david4henery00@hotmail.com>
2022-09-08 15:01:40 +0100

    responsive layout

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -3,7 +3,7 @@ import React from "react";
 const Tab = ({ num, name }) => {
   return (
     <div className="countdown-tab">
-      <p>{num}</p>
+      <p className="countdown-tab__num">{num}</p>
       <p className="countdown-tab__names">
       {name}
       </p>
