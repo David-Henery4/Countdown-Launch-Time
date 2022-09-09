654870ae89ce5718cd0f9358b072bb003bc44b6f
DizzlyD <david4henery00@hotmail.com>
2022-09-09 11:09:52 +0100

    before animation

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -3,10 +3,13 @@ import React from "react";
 const Tab = ({ num, name }) => {
   return (
     <div className="countdown-tab">
-      <p className="countdown-tab__num">{num}</p>
-      <p className="countdown-tab__names">
-      {name}
-      </p>
+      <div className="countdown-upper">
+        <p className="countdown-tab__num">{num}</p>
+      </div>
+      <div className="countdown-downer">
+        <p className="countdown-tab__num">{num}</p>
+      </div>
+      <p className="countdown-tab__names">{name}</p>
     </div>
   );
 };
