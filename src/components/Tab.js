6ce8d1d0a5286f277d55b478be873d3e6c482afe
DizzlyD <david4henery00@hotmail.com>
2022-09-11 19:21:50 +0100

    basic working animation

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,22 +1,45 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Tab = ({ num, name }) => {
+  const[isFlip, setIsFlip] = useState(false)
   //
   const addFlip = () => {
-    //
-    const upper = document.querySelector(".countdown-upper");
-    upper.classList.add("flip");
-    const downer = document.querySelector(".countdown-downer");
-    downer.classList.add("flip-down");
+    setIsFlip(true)
   };
-  // addFlip()
+  //
+  useEffect(() => {
+    addFlip()
+    // console.log("change")
+  }, [num])
+  //
   return (
     <div className="countdown-tab">
       <div className="countdown-upper">
         <p className="countdown-tab__num countdown-upper__num">{num}</p>
       </div>
+      <div
+        className={
+          isFlip ? "countdown-upper-flip flip-top" : "countdown-upper-flip"
+        }
+      >
+        <p className="countdown-tab__num countdown-upper__num">{num}</p>
+      </div>
+      {/* <p className="countdown-tab__num number-middle">{num}</p> */}
       <div className="countdown-downer">
         <p className="countdown-tab__num countdown-downer__num">{num}</p>
+      </div>
+      <div
+        className={
+          isFlip ? "countdown-downer-flip flip-bottom" : "countdown-downer-flip"
+        }
+        onAnimationEnd={() => {
+          setIsFlip(false)
+        }}
+      >
+        {/* <p className="countdown-tab__num countdown-downer__num">
+        </p> */}
       </div>
       <p className="countdown-tab__names">{name}</p>
     </div>
